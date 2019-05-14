f4770706a5c1f9d87e92cbda436881005e5e3f0c
Andre <andre@labtime.ufg.br>
2019-05-14 10:29:23 -0300

    validacao de campo vazio

--- javascript/script.js
+++ javascript/script.js
@@ -4,13 +4,13 @@ function calcula_imc() {
 
     //Formula para calcular IMC
     var imc = peso / (altura * altura);
-    var message = '';
 
-    if (peso == '' || altura == '') {
-        document.getElementById("message").innerHTML = '<h4>Preencha os campos</h4>';
+    if (!peso || !altura) {
+        var error_field = document.getElementById("message")
+        error_field.innerHTML = "<h4>Preencha os campos</h4>"
         return false;
     }
-
+    console.log(imc)
     if (imc < 18.5) {
         alert("Você está abaixo do peso. Indice de massa corporal:  " + imc.toFixed(2));
     } else if (imc >= 18.5 && imc < 24.9) {
@@ -19,12 +19,11 @@ function calcula_imc() {
         alert("Você está levemente acima do peso. Indice de massa corporal:  " + imc.toFixed(2));
     } else if (imc >= 30 && imc < 34.9) {
         alert("Cuidado! Obesidade grau 1. Indice de massa corporal:  " + imc.toFixed(2));
-    } else if (imc >= 35 && imc <= 39, 9) {
+    } else if (imc >= 35 && imc <= 39.9) {
         alert("Cuidado! Obesidade grau 2, obsesidade severa! Indice de massa corporal:  " + imc.toFixed(2))
     } else if (imc > 40)
         alert("Cuidado! Obesidade morbida, procure um médico, você corre risco de vida!!! Indice de massa corporal:  " + imc.toFixed(2));
 
-    output.innerHTML = '<h1>' + imc + '</h1><h4>' + message + '</h4>';
 }
 
 function refresh() {
